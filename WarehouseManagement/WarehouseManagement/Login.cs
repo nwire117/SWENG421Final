1764fbbcfb13513f3030b9cf5cedb337458b0150
Neal Wire <nawire@hotmail.com>
2021-12-08 11:28:19 -0500

    Added login screen.

--- WarehouseManagement/WarehouseManagement/Login.cs
+++ WarehouseManagement/WarehouseManagement/Login.cs
@@ -10,12 +10,26 @@ using System.Windows.Forms;
 
 namespace WarehouseManagement
 {
-    public partial class Form1 : Form
+    public partial class Login : Form
     {
-        public Form1()
+        public Login()
         {
             InitializeComponent();
         }
 
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
