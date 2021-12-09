859680e74f803e36f4abf4bed173092e69e59385
cjdrangel209 <cjdrangel209@gmail.com>
2021-12-09 15:08:50 -0500

    added IFactory, IProduct, and Factory classes

---
+++ WarehouseManagement/WarehouseManagement/Factory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseManagement
+{
+    public class Factory: IFactory
+    {
+        public IProduct createProduct(string type)
+        {
+            Type t = Type.GetType(type);
+            Object o = Activator.CreateInstance(t);
+            IProduct p = (IProduct)o;
+
+            return p;
+        }
+    }
+}
