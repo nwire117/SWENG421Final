2adc3e60d33e422f37e872f5b1eed8db5f21803f
Neal Wire <nawire@hotmail.com>
2021-12-10 12:04:53 -0500

    Changed login screen to selection between employee and customer.
    Added remaining classes and interfaces from UML diagram.

---
+++ WarehouseManagement/WarehouseManagement/IOrderElement.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseManagement
+{
+    interface IOrderElement
+    {
+        public void addItem(IOrderElement item);
+        public void deleteItem(IOrderElement item);
+    }
+}
---
+++ WarehouseManagement/WarehouseManagement/OrderShipped.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseManagement
+{
+    class OrderShipped : Order
+    {
+        private Order parent;
+
+       
+    }
+}
