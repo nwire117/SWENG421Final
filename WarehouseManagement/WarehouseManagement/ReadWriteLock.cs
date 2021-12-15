b4754a7429b4364b4cc6de77498076c84970e525
cjdrangel209 <cjdrangel209@gmail.com>
2021-12-15 17:00:36 -0500

    read/write lock

--- WarehouseManagement/WarehouseManagement/ReadWriteLock.cs
+++ WarehouseManagement/WarehouseManagement/ReadWriteLock.cs
@@ -2,23 +2,54 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace WarehouseManagement
 {
     class ReadWriteLock
     {
-        private int waitingReadLock;
-        private int currentReadLock;
-        private List<IProduct> waitingWriteLock = new List<IProduct>();
+        private int waitingReadLock = 0;
+        private int currentReadLock = 0;
+        private List<Thread> waitingWriteLock = new List<Thread>();
+        private Thread writeLockedThread;
 
         public void readLock()
         {
-
+            if (writeLockedThread != null)
+            {
+                waitingReadLock++;
+                while(writeLockedThread != null)
+                {
+                    
+                }
+                waitingReadLock--;
+            }
+            currentReadLock++;
         }
 
         public void writeLock()
         {
+            Thread thisThread;
+
+            lock (this)
+            {
+                if(writeLockedThread == null && currentReadLock == 0)
+                {
+                    writeLockedThread = Thread.CurrentThread;
+                    return;
+                }
+                thisThread = Thread.CurrentThread;
+                waitingWriteLock.Add(thisThread);
+            }
+
+            lock (thisThread)
+            {
+                while (thisThread != writeLockedThread)
+                {
+                    
+                }
+            }
 
         }
 
